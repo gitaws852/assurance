560d0bdc287b09f8035673db8a53c4b865fa47af
gitaws852 <92790480+gitaws852@users.noreply.github.com>
2021-11-08 09:53:27 +0530

    08_0953
    08_0953

--- test_lotl.py
+++ test_lotl.py
@@ -3,11 +3,11 @@ import uuid
 
 
 crId = str(uuid.uuid4())
-url = "https://api.bureau.id/v2/auth/initiate?countryCode=IN&callbackUrl=https://en4lssdchkyjk.x.pipedream.net/&clientId=" + crId + "&transactionId=a15facc8-0f38-46f4-bac8&mobile=919845045249"
 
-payload={}
-headers = {}
+def test_lotl():
 
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+  url = "https://api.bureau.id/v2/auth/initiate?countryCode=IN&callbackUrl=https://en4lssdchkyjk.x.pipedream.net/&clientId=" + crId + "&transactionId=a15facc8-0f38-46f4-bac8&mobile=919845045249"
+  payload={}
+  headers = {}
+  response = requests.request("GET", url, headers=headers, data=payload)
+  print(response.text)

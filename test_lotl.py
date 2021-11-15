7a05e2f6c29ab23dd9eb80bc91449a64f4515a4f
gitaws852 <92790480+gitaws852@users.noreply.github.com>
2021-11-15 12:15:36 +0530

    15_1215
    15_1215

--- test_lotl.py
+++ test_lotl.py
@@ -5,9 +5,11 @@ import uuid
 crId = str(uuid.uuid4())
 
 #def test_lotl():
-url = "https://api.bureau.id/v2/auth/initiate?countryCode=IN&callbackUrl=https://en4lssdchkyjk.x.pipedream.net/&clientId=babab5b5-404c-4180-8e49-a24b0c924f3c&transactionId=" + crId
+#url = "https://api.bureau.id/v2/auth/initiate?&mobile=779999999995&countryCode=IN&callbackUrl=https://ent4nlzguowt.x.pipedream.net/&clientId=ebd5ed12-5e42-4f30-b793-056d44af9d19&transactionId=" + crId
+url = "https://api.sandbox.bureau.id/v2/auth/initiate?&callbackUrl=https://ent4nlzguowt.x.pipedream.net/&clientId=ebd5ed12-5e42-4f30-b793-056d44af9d19&transactionId=" + crId + "&mobile=919845045249"
 payload={}
 headers = {}
+print(url)
 response = requests.request("GET", url, headers=headers, data=payload)
 assert response.status_code == 200
 print(response.text)
